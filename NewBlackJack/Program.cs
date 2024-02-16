43c27535a37addea09cc9ba2751b1303f2b4fa73
Gabriel Squissato <biel.squissato@hotmail.com>
2024-02-16 00:08:26 -0300

    ajustes

--- NewBlackJack/Program.cs
+++ NewBlackJack/Program.cs
@@ -15,7 +15,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<IBaralhoService, BaralhoService>();
-builder.Services.AddBlazoredModal();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices(config =>
 {
